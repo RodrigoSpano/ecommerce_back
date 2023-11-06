99b69141aa12d882aef26709382fd5c86ae85ab4
RodrigoSpano <rorrospano17@gmail.com>
2023-11-06 19:21:49 -0300

    feature/ jwt middleware for routes

--- src/services/passport/Jwt.strategy.ts
+++
@@ -1,7 +0,0 @@
-import { Strategy, ExtractJwt } from 'passport-jwt'
-import UserModel from '../../models/user.model'
-
-// export default const JwtStrategy = new Strategy({
-//   jwtFromRequestL: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//   secretOrKey: `${process.env.JWT_SECRET}`
-// }, function(){payload, done} => {})
